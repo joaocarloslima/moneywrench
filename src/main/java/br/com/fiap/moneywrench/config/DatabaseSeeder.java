95c54e791828abf34a842229cc6a4932c99b1280
Joao <joaocarloslima@me.com>
2023-11-09 22:55:28 -0300

    fix: server action carregar categorias

--- src/main/java/br/com/fiap/moneywrench/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/moneywrench/config/DatabaseSeeder.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.fiap.moneywrench.model.Categoria;
 import br.com.fiap.moneywrench.model.Movimentacao;
@@ -14,6 +15,7 @@ import br.com.fiap.moneywrench.repository.CategoriaRepository;
 import br.com.fiap.moneywrench.repository.MovimentacaoRepository;
 
 @Configuration
+@Profile("dev")
 public class DatabaseSeeder implements CommandLineRunner {
 
     @Autowired
